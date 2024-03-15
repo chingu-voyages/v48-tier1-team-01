f5203e663c80259f37cefb54f213424f40e09460
Paul <paulcapob@gmail.com>
2024-03-15 15:09:20 +0900

    Updated Diet chart and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import DinoList from './components/DinoList'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import DinoList from './components/DinoList';
+import DietChart from './components/DietChart/DietChart';
 
 function App() {
-  const [dino, setDino] = useState([])
+  const [dino, setDino] = useState([]);
   useEffect(() => {
-    axios.get(`https://chinguapi.onrender.com/dinosaurs`).then(res => setDino(res.data))
-  }, [])
+    axios
+      .get(`https://chinguapi.onrender.com/dinosaurs`)
+      .then((res) => setDino(res.data));
+  }, []);
 
   return (
     <>
       <h1>Welcome to Dinasaur App</h1>
       <DinoList dinoList={dino} />
+      <DietChart />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
