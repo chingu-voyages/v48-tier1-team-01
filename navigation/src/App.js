2fc8438906392866018bd5865b50b60cf4e77a6b
KingNoran <kingnoran8@gmail.com>
2024-03-18 18:23:52 +0800

    Added a working menu to the nav

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -1,12 +1,13 @@
 import dinoLogo from './img/DinoLogo.png';
 import hamBurger from './img/hamburger.png';
+import cross from './img/cross.png';
 import './App.css';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <Nav dinoLogo={dinoLogo} hamBurger={hamBurger}/>
+      <Nav dinoLogo={dinoLogo} hamBurger={hamBurger} cross={cross}/>
     </div>
   );
 }
--- navigation/src/components/Nav.js
+++ navigation/src/components/Nav.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const Nav = (props) => {
+  const [show, setShow] = useState(false)
   return (
-    <nav>
-        <div className='logo'>
-            <img src={props.dinoLogo}></img>
-            <span>chinguDino</span>
-        </div>
-        <ul>
-            <li><a href='#' className="active">Search</a></li>
-            <li><a href='#' className="inactive">Dietchart</a></li>
-        </ul>
-        <div className='hamburger'>
-            <img src={props.hamBurger}></img>
-        </div>
-    </nav>
-  )
+    <>
+      <nav>
+          <div className="header">
+            <div className='logo'>
+              <img src={props.dinoLogo}></img>
+              <span>chinguDino</span>
+            </div>
+            <ul>
+              <li><a href='#' className='active'>Search</a></li>
+              <li><a href='#' className='inactive'>Dietchart</a></li>
+            </ul>
+            <div className='menu' onClick={()=>setShow(!show)}>
+                <img src={
+                  show ? props.cross : props.hamBurger
+                }></img>
+            </div>
+          </div>
+          {
+            show ?
+            <div className="dropDown">
+            <ul>
+              <li className='active'><a href='#'>Search</a></li>
+              <li className='inactive'><a href='#'>Dietchart</a></li>
+            </ul>
+          </div>
+          : null
+          }
+          
+      </nav>
+    </>
+  );
 }
 
 export default Nav;
