e5a63b422f1c905fbbad17090e0e1086d72f5154
KingNoran <kingnoran8@gmail.com>
2024-03-21 21:53:16 +0800

    Created the footer component!

---
+++ footer/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Footer = (props) => {
+  return (
+    <footer>
+      &copy; 2024 <span className="blue">{props.icon}v48-tier1-team01</span> All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
