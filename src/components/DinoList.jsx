e3f580ed3db9e0453a1703b11b568e2296fe4c56
psmacabulos <107623965+psmacabulos@users.noreply.github.com>
2024-03-31 20:49:09 +0800

    adjusted grid size and added hover effects on DinoList

--- src/components/DinoList.jsx
+++ src/components/DinoList.jsx
@@ -2,25 +2,27 @@
 
 const DinoList = ({ dinoQuery, dinoSearch, onSelectedDino }) => {
   return (
-    <ul className='flex flex-wrap justify-center gap-10  '>
-      {dinoQuery
-        .filter(item =>
-          item.name.toLowerCase() === ''
-            ? item
-            : item.name.toLowerCase().includes(dinoSearch.toLowerCase())
-        )
-        .map(dino => (
-          <li
-            key={dino.id}
-            className='flex items-center justify-between flex-col flex-grow  border-solid border-slate-600  w-3/12 shadow-lg shadow-slate-950 p-4 gap-3'
-          >
-            <img src={dino.imageSrc} alt={dino.name} className='m-auto' />
-            <button onClick={() => onSelectedDino(dino.id)} className='text-blue-400 text-2xl'>
-              {dino.name}
-            </button>
-          </li>
-        ))}
-    </ul>
+    <div className='flex justify-center'>
+      <ul className='flex flex-wrap justify-center gap-10 w-9/12'>
+        {dinoQuery
+          .filter(item =>
+            item.name.toLowerCase() === ''
+              ? item
+              : item.name.toLowerCase().includes(dinoSearch.toLowerCase())
+          )
+          .map(dino => (
+            <li
+              key={dino.id}
+              className='hover:scale-110 transition-all flex items-center justify-between flex-col flex-grow  border-solid border-slate-600  w-3/12 shadow-lg shadow-slate-950 p-4 gap-3'
+            >
+              <img src={dino.imageSrc} alt={dino.name} className='m-auto ' />
+              <button onClick={() => onSelectedDino(dino.id)} className='text-blue-800 text-xl'>
+                {dino.name}
+              </button>
+            </li>
+          ))}
+      </ul>
+    </div>
   )
 }
 
